42ef1e0e26e1114c6ce2354f29644718ef39e85b
Devanshu Shyamsundar <132264393+devanshu-0534@users.noreply.github.com>
2025-10-16 16:32:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function firstWord(s) {
   // your code here
+	  s = s.trim();
+    if (s === "") return "";
+  let spaceIndex = s.indexOf(" ");
+  if (spaceIndex === -1) return s;
+  return s.substring(0, spaceIndex);
 }
 
 // Do not change the code below
